7667730c646ed07486f77a7ade70bc7e529f02f4
fjl <18618166564@163.com>
2021-09-09 15:42:09 +0800

    1

---
+++ src/reactive.ts
@@ -0,0 +1,14 @@
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      const res = Reflect.get(target, key)
+      // 收集依赖
+      return res;
+    },
+    set(target, key, value) {
+      const res = Reflect.set(target, key, value);
+      // 触发依赖
+      return res;
+    },
+  })
+}
