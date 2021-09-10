16f9ca114984c2717a64b1358deb8d9dcd6d9bbb
fjl <18618166564@163.com>
2021-09-10 11:57:31 +0800

    t

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -3,49 +3,51 @@ import {effect,stop} from '../effect';
 describe("effect", () => {
   it("happy 1path", () => {
     const user = reactive({
-      age: 10
+      age: 10,
     })
-    let nextAge;
+    let nextAge
     effect(() => {
-      nextAge = user.age + 1;
-
+      nextAge = user.age + 1
     })
 
     expect(nextAge).toBe(11);
     // update
-    user.age++;
+    user.age++
     expect(nextAge).toBe(12)
   })
 
-  it("should return runner when call effect", () => {
-    let foo = 10;
+  it('should return runner when call effect', () => {
+    let foo = 10
     let runner = effect(() => {
-      foo++;
+      foo++
     })
     expect(foo).toBe(11)
     let r = runner()
-  expect(foo).toBe(12)
+    expect(foo).toBe(12)
   })
 
-  it("scheduler", () => {
-    let dummy;
-    let run:any;
+  it('scheduler', () => {
+    let dummy
+    let run: any
     const scheduler = jest.fn(() => {
-      run = runner;
-    });
-    const obj = reactive({foo: 1})
-    const runner = effect(() => {
-      dummy = obj.foo;
-    }, {
-      scheduler
+      run = runner
     })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      {
+        scheduler,
+      }
+    )
     expect(scheduler).not.toHaveBeenCalled()
     expect(dummy).toBe(1)
 
     obj.foo++
     expect(scheduler).toHaveBeenCalledTimes(1)
     expect(dummy).toBe(1)
-    run();
+    run()
     expect(dummy).toBe(2)
   })
 
