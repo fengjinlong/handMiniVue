bfd499bf7b235de63570e0b998f36b33614515d3
fjl <18618166564@163.com>
2021-10-09 17:18:28 +0800

    t

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,7 +1,19 @@
-import { h } from '../../lib/guide-mini-vue.esm.js'
+import {
+  h
+} from '../../lib/guide-mini-vue.esm.js'
 export const APP = {
   render() {
-    return h('div', this.msg)
+    return h('div', {
+      id: 'root',
+      class: ['red', 'head']
+    // string
+    // }, 'hi ' + this.msg
+    // array
+    }, [
+      h('p', {class: 'red'}, 'hi '),
+      h('p', {class: 'green'}, 'mini-vue')
+    ]
+    )
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from '../share/index'
 import { createComponnentInstance, setupComponnent } from './component'
 
 export function render(vnode, container) {
@@ -5,10 +6,38 @@ export function render(vnode, container) {
 }
 
 function patch(vnode: any, container: any) {
-  // 判断vnode是否是element 
-  // processElement() 
-  processComponent(vnode, container)
+  // 判断vnode是否是element
+  console.log(vnode.type)
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
 }
+
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container)
+}
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type)
+  console.log(el)
+  const { children } = vnode
+  if (typeof children === 'string') {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el)
+    
+  }
+
+  const { props } = vnode
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+  console.log(el)
+  container.append(el)
+}
+
 function processComponent(vnode: any, container: any) {
   mountComponent(vnode, container)
 }
@@ -19,7 +48,7 @@ function mountComponent(vnode: any, container: any) {
 
   // 初始化组件，初始化一些属性挂载
   setupComponnent(instance)
-  
+
   setupRenderEffect(instance, container)
 }
 function setupRenderEffect(instance: any, container: any) {
@@ -28,6 +57,9 @@ function setupRenderEffect(instance: any, container: any) {
   // vnode -element - mountElement;
   patch(subTree, container)
 }
-
-
+function mountChildren(vnode: any, container: any) {
+  vnode.children.forEach(v => {
+    patch(v, container)
+  })
+}
 
