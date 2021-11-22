995d3b1836c12262f73a97867d4a2875a17b7158
fjl <18618166564@163.com>
2021-11-22 22:45:14 +0800

    update

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -15,6 +15,7 @@ export function createComponentInstance(vnode, parent) {
     slots: {},
     parent,
     isMounted: false,
+    subTree: {},
     provides: parent ? parent.provides : {},
     emit: () => {},
   }
